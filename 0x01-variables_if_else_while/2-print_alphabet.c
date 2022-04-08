cbefbbf8262e59ad5ae8d231bbd521fcef63f7c2
Fanuel369 <102294758+Fanuel369@users.noreply.github.com>
2022-04-08 23:10:35 +0100

    C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
  * main 
- * competition
+ * Description: print lowercase alphabets
  * Return: Always (Success)
  */
 int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+	for (c = 0; c < 26; c++)
+        putchar('a' + c);
 
 	putchar('\n');
+
 	return (0);
 }
