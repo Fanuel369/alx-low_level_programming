2d98129376f83e3809b997c7c03627d501d2458d
Fanuel369 <102294758+Fanuel369@users.noreply.github.com>
2022-04-08 23:43:23 +0100

    C

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 /**
-  * main 
-  * Description: C programming
-  * Return: Always (Success)
-  */
+ *main - entry point
+ *
+ *Description:prints alphabet in reverse
+ *
+ *Return: Always zero
+ */
 int main(void)
 {
-	char c;
-	for (c = 'z'; c >= 'a'; c--)
+	int letter, n;
+        for (letter = 122, n = 1; n < 27; letter--, n++)
 	{
-		putchar(c);
+		putchar(letter);
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
+
+
