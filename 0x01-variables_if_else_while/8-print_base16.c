38de714e674f0f5da8996fb77ab1c96f6f90e73b
Fanuel369 <102294758+Fanuel369@users.noreply.github.com>
2022-04-09 00:00:02 +0100

    C

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 /**
-  * main
-  * Description: programming
-  * Return: Always (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int dig, letterhex, n;
-
-	for (dig = 48, n = 0; n < 10; dig++, n++)
-	{
-		putchar(dig);
-	}
-	for (letterhex = 97, n = 1; n < 7; letterhex++, n++)
-	{
-		putchar(letterhex);
-	}
-	putchar(10)
-	return (0);
+int z;
+for (z = 48; z <= 57; z++)
+{
+putchar(z);
+}
+for (z = 97; z <= 102;z++)
+{
+putchar(z)
+}
+putchar('\n');
+return (0);
 }
